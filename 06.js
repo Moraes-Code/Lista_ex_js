62c5d16116318d00038181edb3e93457bf5d574b
Moraes-Code <bruno_l_moraes@estudante.sesisenai.org.br>
2026-03-20 08:10:00 -0300

    ADD - Fazendo 7 e 8 e utilizando varialvel nova

---
+++ 06.js
@@ -0,0 +1,17 @@
+function vogalOuConsoante(){
+
+    let letra = prompt("Digite um letra: ");
+
+    if(
+
+letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"
+
+    ){
+        console.log("vogal");
+
+    }else{
+        console.log("consoante");
+    }
+
+}
+
---
+++ 07.js
@@ -0,0 +1,15 @@
+function picoles(){
+    let codigo = prompt("Digite o codigo do picole: ");
+
+    switch(codigo){
+            case "a ":
+            console.log("Chocolate = 10 pila")
+            break;
+            case "b ":
+            console.log("Chocolate = 10 pila")
+            break;
+            case "c ":
+            console.log("Chocolate = 10 pila")
+            break;
+    }
+}
